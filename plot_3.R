7574112058f3c970bc92fde7c42016a08788f920
michem <thomas.michem@gmail.com>
2015-02-08 19:59:20 +0100

    fix legend error on plot 3 and 4

--- plot_3.R
+++ plot_3.R
@@ -26,7 +26,7 @@ plot(DateTime, Sub_metering_1, type = 'l', xlab='', ylab = 'Energy sub metering'
 lines(DateTime,Sub_metering_2,col="red")
 lines(DateTime,Sub_metering_3,col="blue")
 legend("topright", 
-       c("Sub_metering_1","Sub_metering_3","Sub_metering_3"), 
+       c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
        lty=c(1,1,1), # symbols (lines)
        lwd=c(2.5,2.5),col=c("black", "red","blue")) 
 dev.copy(png, file = "plot_3.png")
